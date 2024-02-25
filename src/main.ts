54ed14d8d4534f33ead67ebcb4266609c06be1de
penghs <penghs@agilean.cn>
2024-02-25 09:34:34 +0800

    全局注册element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+app.use(ElementPlus, {
+    locale: zhCn
+})
 
+app.use(ElementPlus)
 app.mount('#app')
