a767627e7496b865e478400a9fe1345b1c5e8b0b
penghs <penghs@agilean.cn>
2024-02-25 13:54:56 +0800

    svg测试

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <el-button type="primary">I am ElButton</el-button>
+  <svg>
+    <use xlink:href="#icon-phone"></use>
+  </svg>
 </template>
 
 <script setup lang="ts">
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default () => {
+  return {
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
-})
+}
