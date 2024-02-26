64f44a14ad302bc8e1b596be28fac8d6cfd5962e
penghs <penghs@agilean.cn>
2024-02-26 20:17:41 +0800

    fix：解决登录表单无法输入的问题

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,38 +1,39 @@
 <template>
   <div class="login_container">
     <el-row>
-      <!-- xs表示当屏幕宽度小于 768px 时,此时显式0个栅格,也就是不显式左侧布局,右侧占满 -->
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
         <!-- 登录的表单 -->
-        <el-form class="login_form" ref="loginForms" label-width="auto" label-position="top">
+        <el-form class="login_form" :model="loginForm" ref="loginForms">
           <div class="logo">
             <SvgIcon name="logo" width="80px" height="80px"></SvgIcon>
-            <h2>Nuturbo后台管理</h2>
+            <h2>Nuturbo零代码平台</h2>
           </div>
           <el-form-item prop="username">
-            <el-input :prefix-icon="User" v-model="loginForms.username" placeholder="请输入手机号/邮箱/用户名"></el-input>
+            <!-- 如果不绑定数据的话，无法输入 -->
+            <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
           </el-form-item>
           <el-form-item prop="password">
-            <el-input :prefix-icon="Lock" type="password" v-model="loginForms.password" show-password placeholder="请输入密码"></el-input>
+            <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button class="login_btn" type="primary" size="default">登录</el-button>
+            <el-button  class="login_btn" type="primary" size="default">登录</el-button>
           </el-form-item>
         </el-form>
       </el-col>
     </el-row>
   </div>
 </template>
-  
+
+
 
 <script setup lang='ts'>
 import SvgIcon from '@/components/icon/SvgIcon.vue';
 import { User,Lock } from '@element-plus/icons-vue';
 import { reactive } from 'vue';
 
-let loginForms = reactive({username:'',password:''})
-
+let loginForm = reactive({username:'',password:''})
+console.log(loginForm)
 
 </script>
 
@@ -41,7 +42,7 @@ let loginForms = reactive({username:'',password:''})
 .login_container {
   width: 100%;
   height: 100vh;
-  //background: url('@/assets/images/background.jpg') no-repeat;
+  background: url('@/assets/images/background.jpg') no-repeat;
   background-size: cover; //cover是让图片铺满整个容器
 
   .login_form {
@@ -49,7 +50,7 @@ let loginForms = reactive({username:'',password:''})
     width: 40%;
     top: 25vh; //vh是视口高度的百分比，需要position: relative
     left: 12vw;
-    //background: url("@/assets/images/login_form.png") no-repeat;
+    background: url("@/assets/images/login_form.png") no-repeat;
     background-size: cover;
     padding: 40px;
 
